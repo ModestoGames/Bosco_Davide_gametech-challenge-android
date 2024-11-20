22aeed867d1374da54bf22e3de654a787c6a45b1
Davide Bosco <info.modestogames@gmail.com>
2024-11-20 17:51:44 +0100

    android-remove-notifications

--- notification_module/src/main/java/com/modesto/notification_module/LocalNotificationsService.java
+++ notification_module/src/main/java/com/modesto/notification_module/LocalNotificationsService.java
@@ -7,6 +7,12 @@ import android.content.Context;
 import android.os.Build;
 import androidx.work.OneTimeWorkRequest;
 import androidx.work.WorkManager;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -18,18 +24,26 @@ public class LocalNotificationsService extends UnityService{
         super(unityActivity);
     }
 
+    //keep track of the scheduled notifications to handle them during app runtime
+    private Map<Integer, UUID> scheduledNotifications = new HashMap<>();
     /**
      * Initializes the service. Creates a notification channel if the device is running
      * Android Oreo (API 26) or above. Channels are mandatory for notifications on these versions.
      */
     @Override
     public void OnInitialize() {
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             NotificationChannel channel = new NotificationChannel(
                     Constants.CHANNEL_ID,
                     Constants.CHANNEL_NAME,
                     NotificationManager.IMPORTANCE_HIGH
             );
+
+            channel.enableVibration(true);
+            // vibration schema (initial delay, vibration, pause, vibration)
+            long[] vibrationPattern = {0, 500, 250, 500};
+            channel.setVibrationPattern(vibrationPattern);
 
             NotificationManager manager = (NotificationManager) _unityActivity.getSystemService(Context.NOTIFICATION_SERVICE);
             manager.createNotificationChannel(channel);
@@ -39,14 +53,34 @@ public class LocalNotificationsService extends UnityService{
     public void scheduleNotification(int id, int delayMinutes) {
         // Create a one-time work request with the delay specified.
         OneTimeWorkRequest notificationWork = new OneTimeWorkRequest.Builder(NotificationWorker.class)
-                .setInitialDelay(delayMinutes, TimeUnit.MINUTES)
-                .setInputData(
-                        new androidx.work.Data.Builder()
-                                .putInt("id", id)
-                                .build()
-                )
-                .build();
+            .setInitialDelay(delayMinutes, TimeUnit.MINUTES)
+            .setInputData(
+                new androidx.work.Data.Builder()
+                        .putInt("id", id)
+                        .build()
+            )
+            .build();
+
+        //cache reference to the scheduledNotification list
+        scheduledNotifications.put(id, notificationWork.getId());
         // Enqueue the work request to WorkManager.
         WorkManager.getInstance(_unityActivity).enqueue(notificationWork);
+    }
+
+    //retrieve a scheduled work UUID and use it to delete the work
+    public void deleteScheduledNotification(int id)
+    {
+        UUID workId = scheduledNotifications.get(id);
+        WorkManager.getInstance(_unityActivity).cancelWorkById(workId);
+        scheduledNotifications.remove(id);
+    }
+
+    //iterate over scheduled notification work id and delete them all
+    public void deleteAllScheduledNotifications() {
+        //create a list for iteration to avoid ConcurrentModificationException
+        List<Integer> ids = new ArrayList<>(scheduledNotifications.keySet());
+        for (Integer id : ids) {
+            deleteScheduledNotification(id);
+        }
     }
 }
